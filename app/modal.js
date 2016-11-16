25edef8b9750516d404963735357192db692f7d5
CliffLee <leecliff@usc.edu>
2016-11-15 23:10:48 -0800

    some bugs with modal.js and jquery selectors

--- app/modal.js
+++ app/modal.js
@@ -12,25 +12,29 @@ var span = document.getElementsByClassName("close")[0];
 // When the user clicks on the button, open the modal
 loginBtn.onclick = function() {
     $('#modal').load("partials/loginModal.html", closeModal);
-    modal.style.display = "block";
+    $('#modal').css("display", "block");
+    // modal.style.display = "block";
 }
 
 uploadBtn.onclick = function() {
     $('#modal').load("partials/uploadModal.html", closeModal);
-    modal.style.display = "block";
+    $('#modal').css("display", "block");
+    // modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 var closeModal = function() {
     var span = document.getElementsByClassName("close")[0];
     span.onclick = function() {
-        modal.style.display = "none";
+        $('#modal').css("display", "none");
+        // modal.style.display = "none";
     }
 
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function(event) {
         if (event.target == modal) {
-            modal.style.display = "none";
+            $('#modal').css("display", "none");
+            // modal.style.display = "none";
         }
     }
 }

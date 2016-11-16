8c6d5770c8109ceaf255fb193716370fc7e98b58
CliffLee <leecliff@usc.edu>
2016-11-15 23:05:02 -0800

    disgusting solution to an annoying problem; mysolutionisbadandishouldfeelbad.jpg

--- app/modal.js
+++ app/modal.js
@@ -1,45 +1,41 @@
-// var modal           = document.getElementById('modal');
-//
-// var loginBtn        = document.getElementById('loginButton');
-// var uploadBtn        = document.getElementById('uploadButton');
-//
-// // Get the <span> element that closes the modal
-// var span = document.getElementsByClassName("close")[0];
-// $('#modal').load("partials/loginModal.html");
-//
-// // When the user clicks on the button, open the modal
-// loginBtn.onclick = function() {
-//
-//     modal.style.display = "block";
-// }
-//
-// uploadBtn.onclick = function() {
-//     // $('#modal').load("partials/uploadModal.html");
-//     modal.style.display = "block";
-// }
-//
-// // When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-//     modal.style.display = none;
-// }
-//
-// // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//     if (event.target == modal) {
-//         modal.style.display = "none";
-//     }
-// }
+var modal           = document.getElementById('modal');
 
-$(function() {
-    $("#uploadButton").click(function() {
-        $("#modal").load("partials/uploadModal.html");
-        $("#modal").toggleClass("modal-active");
-    });
-    $("#loginButton").click(function() {
-        $("#modal").load("partials/loginModal.html");
-        $("#modal").toggleClass("modal-active");
-    });
-    $("#close").click(function() {
-        $("#modal").removeClass("modal-active");
-    });
-});
+var loginBtn        = document.getElementById('loginButton');
+var uploadBtn       = document.getElementById('uploadButton');
+var friendsBtn      = document.getElementById('friendsButton');
+var enrollBtn       = document.getElementById('enrollButton');
+var commonBtn       = document.getElementById('commonButton');
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+loginBtn.onclick = function() {
+    $('#modal').load("partials/loginModal.html", closeModal);
+    modal.style.display = "block";
+}
+
+uploadBtn.onclick = function() {
+    $('#modal').load("partials/uploadModal.html", closeModal);
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+var closeModal = function() {
+    var span = document.getElementsByClassName("close")[0];
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
+//
+// span.onclick = function() {
+//     console.log("wtf");
+//     modal.style.display = "none";
+// }

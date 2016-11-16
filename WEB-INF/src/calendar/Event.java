e5f104db44bf242fd906d3c864c0695cbe100189
Vadim Korolik <korolik@usc.edu>
2016-11-15 16:33:50 -0800

    adding event database integration

--- WEB-INF/src/calendar/Event.java
+++ WEB-INF/src/calendar/Event.java
@@ -8,31 +8,39 @@ import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.Vector;
 
+import com.google.gson.Gson;
+
 import user.User;
 import util.Util;
+
 //import java.util.Date;
 
 public class Event {
+	public final class RelationshipTypes {
+		public final static String OWNED = "owned";
+		public final static String SHARED = "shared";
+	}
 	 
 	private long id;
 	private String name;
-	private Timestamp datetime; 
+	private Timestamp startTimestamp, endTimestamp; 
 	private String location, description; 
 	private Color color; 
 	private boolean notify;
 	
-	private Vector<User> shared;
+	private transient Vector<User> shared;
 	
 	public Event() {
 		shared = new Vector<User>();
 	}
 	
 	// used from Calendar when loading all events from db
-	public Event(long id, String name, Timestamp datetime, String location, String description, Color color, boolean notify) {
+	public Event(long id, String name, Timestamp start, Timestamp end, String location, String description, Color color, boolean notify) {
 		this();
 		setId(id);
 		setName(name);
-		setDatetime(datetime);
+		setStartTimestamp(start);
+		setEndTimestamp(end);
 		setLocation(location);
 		setDescription(description);
 		setColor(color);
@@ -40,12 +48,97 @@ public class Event {
 		loadSharedWith();
 	}
 
-	public void parse(String json) {
-		
+	public static Event parse(String json) {
+		Event e = new Gson().fromJson(json, Event.class);
+		e.loadSharedWith();
+		return e;
 	}
 	
-	public void write() {
-		
+	public String toJson() {
+		return new Gson().toJson(this);
+	}
+	
+	public void write(User current_user) {
+		Connection conn = null;
+		try {
+			conn = Util.getConn();
+			conn.setAutoCommit(false);
+			
+			PreparedStatement st = conn.prepareStatement("SELECT id FROM Events WHERE id=?");
+			st.setLong(0, id);
+			ResultSet rs = st.executeQuery();
+			PreparedStatement st2 = null;
+			if (rs.next()) {
+				// event exists, update
+				st2 = conn.prepareStatement("UPDATE Events SET name=?, start_time=?, end_time=?, location=?, color=?, notify=? WHERE id=?");
+				st2.setLong(7, id);
+			} else {
+				// event does not exist
+				st2 = conn.prepareStatement("INSERT INTO Events (name, start_time, end_time, location, color, notify) VALUES (?, ?, ?, ?, ?, ?)");
+			}
+			st2.setString(1, name);
+			st2.setTimestamp(2, startTimestamp);
+			st2.setTimestamp(3, endTimestamp);
+			st2.setString(4, location);
+			st2.setString(5, getColorName());
+			st2.setBoolean(6, notify);
+			st2.executeUpdate();
+			
+			// write own relationship if noone owns this event
+			st2 = conn.prepareStatement("SELECT 0 FROM EventRelationships WHERE event_id=? and relationship_type=?");
+			st2.setLong(1, id);
+			st2.setString(2, RelationshipTypes.OWNED);
+			if (!st2.executeQuery().next()) 
+				createRelationship(conn, current_user, RelationshipTypes.OWNED);
+
+			// write shared relationships
+			for (User u: shared) {
+				createRelationship(conn, u, RelationshipTypes.SHARED);
+			}
+			conn.commit();
+			conn.setAutoCommit(true);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (conn != null)
+					conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	private void createRelationship(Connection conn, User user, String type) throws SQLException {
+		try {
+			PreparedStatement st = conn.prepareStatement("INSERT INTO EventRelationships VALUES (?, ?, ?)");
+			st.setLong(1, user.getId());
+			st.setLong(2, id);
+			st.setString(3, type);
+		} catch (SQLException e) {
+			// INSERT IF NOT EXISTS by virtue of unique constraint 
+			if (e.getErrorCode() != 1062)
+				throw e;
+		}
+	}
+	
+	private String getColorName() {
+		// 'red','orange','yellow','green','blue','cyan','magenta'
+		if (color.equals(Color.RED)) 
+			return "red";
+		else if (color.equals(Color.ORANGE))
+			return "orange";
+		else if (color.equals(Color.YELLOW))
+			return "yellow";
+		else if (color.equals(Color.GREEN))
+			return "green";
+		else if (color.equals(Color.CYAN))
+			return "cyan";
+		else if (color.equals(Color.MAGENTA))
+			return "magenta";
+		else
+			return "blue";
 	}
 	
 	private void loadSharedWith() {
@@ -78,12 +171,20 @@ public class Event {
 		this.name = name;
 	}
 
-	public Timestamp getDatetime() {
-		return datetime;
+	public Timestamp getStartTimestamp() {
+		return startTimestamp;
+	}
+	
+	public Timestamp getEndTimestamp() {
+		return endTimestamp;
 	}
 
-	public void setDatetime(Timestamp datetime) {
-		this.datetime = datetime;
+	public void setStartTimestamp(Timestamp ts) {
+		this.startTimestamp = ts;
+	}
+	
+	public void setEndTimestamp(Timestamp ts) {
+		this.endTimestamp = ts;
 	}
 
 	public String getLocation() {
--- WEB-INF/src/user/User.java
+++ WEB-INF/src/user/User.java
@@ -11,12 +11,7 @@ import java.sql.SQLException;
 import util.Util;
 
 public class User {
-	String username = null;
-	String email = null;
-	String password = null;
-
 	final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
-
 	public static String bytesToHex(byte[] bytes) {
 		char[] hexChars = new char[bytes.length * 2];
 		for (int j = 0; j < bytes.length; j++) {
@@ -26,7 +21,14 @@ public class User {
 		}
 		return new String(hexChars);
 	}
-	
+	public static User getUser(Connection con, long user_id) throws SQLException {
+		PreparedStatement s = con.prepareStatement("SELECT username, password, email FROM Users WHERE id=?");
+		s.setLong(1, user_id);
+		ResultSet rs = s.executeQuery();
+		if (rs.next())
+			return new User(rs.getString(1), rs.getString(2), rs.getString(3));
+		return null;
+	}
 	private static String hashPassword(String password) {
 		try {
 			MessageDigest md = MessageDigest.getInstance("SHA-512");
@@ -40,32 +42,111 @@ public class User {
 		}
 		return null;
 	}
+
+	String email = null;
+	String password = null;
+	String username = null;
+	long id = -1;
 	
-	public static User getUser(Connection con, long user_id) throws SQLException {
-		PreparedStatement s = con.prepareStatement("SELECT username, password, email FROM Users WHERE id=?");
-		s.setLong(1, user_id);
-		ResultSet rs = s.executeQuery();
-		if (rs.next())
-			return new User(rs.getString(1), rs.getString(2), rs.getString(3));
-		return null;
-	}
-	
-	public User() {};
+	public User() {}
 	
 	public User(String username, String raw_password, String email) {
 		setUsername(username);
 		setPassword(raw_password);
 		setEmail(email);
 	}
+	
+	public void clear() {
+		this.username = null;
+		this.password = null;
+		this.email = null;
+	};
+	
+	@Override
+	public boolean equals(Object o) {
+		if (o instanceof User) {
+			return ((User) o).username.equals(username);
+		}
+		return false;
+	}
 
-	public void setUsername(String value) {
-		username = value;
+	public String getEmail() {
+		return email;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+	
+	private ResultSet getUser(Connection con, String username, String raw_password) throws SQLException {
+		PreparedStatement s = con.prepareStatement("SELECT * FROM Users WHERE username=? AND password=?");
+		s.setString(1, username);
+		s.setString(2, hashPassword(raw_password));
+		return s.executeQuery();
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	@Override
+	public int hashCode() {
+		return username.hashCode();
+	}
+
+	public boolean isLoggedIn() {
+		return (username != null);
+	}
+
+	public boolean login(String username, String password) {
+		if (username == null || password == null)
+			return false;
+		if (username.length() == 0 || password.length() == 0)
+			return false;
+		Connection con = null;
+		try {
+			con = Util.getConn();
+			ResultSet rs = getUser(con, username, password);
+			if (rs.next()) {
+				// assert only one such user
+				if (rs.isFirst() && rs.isLast()) {
+					setId(rs.getLong(1));
+					setUsername(username);
+					setPassword(password);
+					setEmail(rs.getString(4));
+					return true;
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (con != null)
+					con.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		setId(-1);
+		setUsername(null);
+		setPassword(null);
+		setEmail(null);
+		return false;
 	}
 
 	public void setEmail(String value) {
 		email = value;
 	}
-
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+	
 	public void setPassword(String password) {
 		if (password == null) {
 			this.password = null;
@@ -74,27 +155,14 @@ public class User {
 		}
 	}
 	
-	public void clear() {
-		this.username = null;
-		this.password = null;
-		this.email = null;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public String getPassword() {
-		return password;
+	public void setUsername(String value) {
+		username = value;
 	}
 
 	public boolean write() {
+		Connection con = null;
 		try {
-			Connection con = Util.getConn();
+			con = Util.getConn();
 			PreparedStatement s = con.prepareStatement("SELECT * FROM Users WHERE username=? OR email=?");
 			s.setString(1, username);
 			s.setString(2, email);
@@ -106,47 +174,20 @@ public class User {
 				s.setString(2, password);
 				s.setString(3, email);
 				s.executeUpdate();
-				return true;
+				// after we write the user, re-login so we can initialize our new ID
+				return login(username, password);
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
-		}
-		return false;
-	}
-
-	public boolean login(String username, String password) {
-		if (username == null || password == null)
-			return false;
-		if (username.length() == 0 || password.length() == 0)
-			return false;
-		try {
-			ResultSet rs = getUser(Util.getConn(), username, password);
-			if (rs.next()) {
-				// assert only one such user
-				if (rs.isFirst() && rs.isLast()) {
-					setUsername(username);
-					setPassword(password);
-					setEmail(rs.getString(4));
-					return true;
-				}
+		} finally {
+			try {
+				if (con != null)
+					con.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-		} catch (SQLException e) {
-			e.printStackTrace();
 		}
-		setUsername(null);
-		setPassword(null);
-		setEmail(null);
 		return false;
-	}
-	
-	public boolean isLoggedIn() {
-		return (username != null);
-	}
-	
-	private ResultSet getUser(Connection con, String username, String raw_password) throws SQLException {
-		PreparedStatement s = con.prepareStatement("SELECT * FROM Users WHERE username=? AND password=?");
-		s.setString(1, username);
-		s.setString(2, hashPassword(raw_password));
-		return s.executeQuery();
 	}
 }

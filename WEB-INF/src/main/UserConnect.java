d9fc5f4febccd12d2539c35336ea34ffe6950e53
Gautam Paranjape <paranjape_gautam@yahoo.com>
2016-11-18 09:00:16 -0800

    remove a user from hashmap when session is closed

--- WEB-INF/src/main/UserConnect.java
+++ WEB-INF/src/main/UserConnect.java
@@ -1,24 +1,18 @@
 package main;
 
+import java.io.IOException;
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArraySet;
-import java.util.Set;
+
 import javax.servlet.http.HttpSession;
+import javax.websocket.EndpointConfig;
 import javax.websocket.OnClose;
 import javax.websocket.OnError;
-import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
-import javax.websocket.EncodeException;
-import javax.websocket.EndpointConfig;
-import java.util.Map;
-import java.io.IOException;
-import java.util.HashMap;
-import main.WebsocketConfiguration;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 
 import user.User;
 
@@ -45,13 +39,19 @@ public class UserConnect {
     @OnClose
         public void close(Session session) {
             allUsers.remove(this);
-        }
+            //remove that user from the hashmap of user to sessions
+            for(Map.Entry<User, Session> entry : userToSession.entrySet()){
+            	if(session.getId().equals(entry.getValue().getId())){
+            		User tmp = entry.getKey();
+            		userToSession.remove(tmp);
+            	}
+            }
+         }
 
     @OnError
         public void onError(Throwable error) throws Throwable{
             System.out.println("Error: " + error.toString());
         }
-
     	public static void sendJSONtoClient(User toReceive, String eventJSON){
     		//TODO send a json to the specific client based on info in a json received
             if(!userToSession.isEmpty()){

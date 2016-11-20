db49e04c52dcddd998d598be7e4b2df4b38b1c26
CliffLee <leecliff@usc.edu>
2016-11-19 17:25:11 -0800

    Event creation functionality

--- app/createEvent.js
+++ app/createEvent.js
@@ -17,7 +17,14 @@ var friendSource = [
 var friendDatalist = document.getElementById("modal-createEvent-friends-list");
 var friendInput = document.getElementById("modal-createEvent-friends-searchbar");
 
-/* FOR WHEN WE DO THE BACKEND FRIENDLIST INTEGRATION
+var title = document.getElementById("eventTitle");
+var startTime = document.getElementById("startTime");
+var startDate = document.getElementById("startDate");
+var endTime = document.getElementById("endTime");
+var endDate = document.getElementById("endDate");
+
+
+// FOR WHEN WE DO THE BACKEND FRIENDLIST INTEGRATION
 var request = new XMLHttpRequest();
 
 request.onreadystatechange = function(res) {
@@ -26,23 +33,32 @@ request.onreadystatechange = function(res) {
             var jsonOptions = JSON.parse(request.responseText);
 
             jsonOptions.forEach(function(item) {
-                var option = document.createElement('option');
-                option.value = item;
-                friendDatalist.appendChild(option);
+                if (item.status === 'accepted') {
+                    var option = document.createElement('option');
+                    option.value = item.username;
+                    friendDatalist.appendChild(option);
+                }
             });
-
-            input.placeholder = "Search through friends";
+            friendInput.placeholder = "Search through friends";
         } else {
-            input.placeholder = "Couldn't load friends :o(";
+            friendInput.placeholder = "Couldn't load friends :o(";
         }
     }
 };
-*/
 
-friendSource.forEach(function(friend) {
-    var option = document.createElement('option');
-    option.value = friend;
-    friendDatalist.appendChild(option);
-});
+request.open('GET', '/friends', true);
+request.send();
+
+
+
+function debug() {
+    console.log(
+        title.value + " " +
+        startTime.value + " " +
+        startDate.value + " " +
+        endTime.value + " " +
+        endDate.value
+    );
+}
 
 /* createEvent.js EOF */

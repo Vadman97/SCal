f27bc2e634817f036d6db92e8d261dbe1007e755
Vadim Korolik <vkorolik@gmail.com>
2016-11-20 00:02:03 -0800

    adding updated sql dump

--- cs201_fp.sql
+++ cs201_fp.sql
@@ -18,7 +18,6 @@
 --
 -- Table structure for table `Discussions`
 --
-USE cs201_fp;
 
 DROP TABLE IF EXISTS `Discussions`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -38,7 +37,7 @@ CREATE TABLE `Discussions` (
   UNIQUE KEY `Column 2` (`section_id`),
   KEY `FK_Discussions_USCClasses` (`class_id`),
   CONSTRAINT `FK_Discussions_USCClasses` FOREIGN KEY (`class_id`) REFERENCES `USCClasses` (`class_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -47,6 +46,7 @@ CREATE TABLE `Discussions` (
 
 LOCK TABLES `Discussions` WRITE;
 /*!40000 ALTER TABLE `Discussions` DISABLE KEYS */;
+INSERT INTO `Discussions` VALUES (1,28980,1,'16:20:00','17:10:00','\0','\0','','\0','\0');
 /*!40000 ALTER TABLE `Discussions` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -60,7 +60,7 @@ DROP TABLE IF EXISTS `EnrolledClasses`;
 CREATE TABLE `EnrolledClasses` (
   `user_id` bigint(20) NOT NULL,
   `class_id` bigint(20) NOT NULL,
-  KEY `FK_EnrolledClasses_Users` (`user_id`),
+  UNIQUE KEY `user_id_class_id` (`user_id`,`class_id`),
   KEY `FK_EnrolledClasses_USCClasses` (`class_id`),
   CONSTRAINT `FK_EnrolledClasses_USCClasses` FOREIGN KEY (`class_id`) REFERENCES `USCClasses` (`class_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `FK_EnrolledClasses_Users` FOREIGN KEY (`user_id`) REFERENCES `Users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
@@ -73,7 +73,64 @@ CREATE TABLE `EnrolledClasses` (
 
 LOCK TABLES `EnrolledClasses` WRITE;
 /*!40000 ALTER TABLE `EnrolledClasses` DISABLE KEYS */;
+INSERT INTO `EnrolledClasses` VALUES (3,1);
 /*!40000 ALTER TABLE `EnrolledClasses` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `EnrolledDiscussions`
+--
+
+DROP TABLE IF EXISTS `EnrolledDiscussions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `EnrolledDiscussions` (
+  `user_id` bigint(20) NOT NULL,
+  `discussion_id` bigint(20) NOT NULL,
+  UNIQUE KEY `user_id_discussion_id` (`user_id`,`discussion_id`),
+  KEY `FK_EnrolledClasses_Users` (`user_id`),
+  KEY `FK_EnrolledClasses_USCClasses` (`discussion_id`),
+  CONSTRAINT `EnrolledDiscussions_ibfk_1` FOREIGN KEY (`discussion_id`) REFERENCES `Discussions` (`discussion_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `EnrolledDiscussions_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `Users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `EnrolledDiscussions`
+--
+
+LOCK TABLES `EnrolledDiscussions` WRITE;
+/*!40000 ALTER TABLE `EnrolledDiscussions` DISABLE KEYS */;
+INSERT INTO `EnrolledDiscussions` VALUES (3,1);
+/*!40000 ALTER TABLE `EnrolledDiscussions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `EnrolledLabs`
+--
+
+DROP TABLE IF EXISTS `EnrolledLabs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `EnrolledLabs` (
+  `user_id` bigint(20) NOT NULL,
+  `lab_id` bigint(20) NOT NULL,
+  UNIQUE KEY `user_id_lab_id` (`user_id`,`lab_id`),
+  KEY `FK_EnrolledClasses_Users` (`user_id`),
+  KEY `FK_EnrolledClasses_USCClasses` (`lab_id`),
+  CONSTRAINT `EnrolledLabs_ibfk_1` FOREIGN KEY (`lab_id`) REFERENCES `Labs` (`lab_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `EnrolledLabs_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `Users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `EnrolledLabs`
+--
+
+LOCK TABLES `EnrolledLabs` WRITE;
+/*!40000 ALTER TABLE `EnrolledLabs` DISABLE KEYS */;
+INSERT INTO `EnrolledLabs` VALUES (3,2);
+/*!40000 ALTER TABLE `EnrolledLabs` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -87,7 +144,7 @@ CREATE TABLE `EventRelationships` (
   `user_id` bigint(20) NOT NULL,
   `event_id` bigint(20) NOT NULL,
   `relationship_type` enum('owned','shared') NOT NULL DEFAULT 'owned',
-  KEY `FK_EventRelationships_Users` (`user_id`),
+  UNIQUE KEY `user_id_event_id_relationship_type` (`user_id`,`event_id`),
   KEY `FK_EventRelationships_Events` (`event_id`),
   CONSTRAINT `FK_EventRelationships_Events` FOREIGN KEY (`event_id`) REFERENCES `Events` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `FK_EventRelationships_Users` FOREIGN KEY (`user_id`) REFERENCES `Users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
@@ -100,6 +157,7 @@ CREATE TABLE `EventRelationships` (
 
 LOCK TABLES `EventRelationships` WRITE;
 /*!40000 ALTER TABLE `EventRelationships` DISABLE KEYS */;
+INSERT INTO `EventRelationships` VALUES (3,29,'owned'),(3,30,'owned'),(3,39,'owned'),(3,40,'owned'),(6,41,'owned'),(7,43,'owned');
 /*!40000 ALTER TABLE `EventRelationships` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -113,12 +171,14 @@ DROP TABLE IF EXISTS `Events`;
 CREATE TABLE `Events` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL DEFAULT 'New Event',
-  `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `location` varchar(255) DEFAULT NULL,
-  `color` enum('red','orange','yellow','green','blue','indigo','violet','cyan','magenta') NOT NULL DEFAULT 'blue',
+  `start_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `end_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `location` varchar(255) NOT NULL DEFAULT '',
+  `description` varchar(255) NOT NULL DEFAULT '',
+  `color` enum('red','orange','yellow','green','blue','cyan','magenta') NOT NULL DEFAULT 'blue',
   `notify` bit(1) NOT NULL DEFAULT b'1',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=44 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -127,6 +187,7 @@ CREATE TABLE `Events` (
 
 LOCK TABLES `Events` WRITE;
 /*!40000 ALTER TABLE `Events` DISABLE KEYS */;
+INSERT INTO `Events` VALUES (29,'test event','2016-11-16 13:50:33','2016-11-16 15:40:00','HNB','my more swag event','red',''),(30,'test event','2016-11-16 13:50:33','2016-11-16 15:40:00','basement lol','','blue',''),(32,'test event','2016-11-16 13:50:33','2016-11-16 15:40:00','basement lol','','blue',''),(33,'test event','2016-11-16 13:50:33','2016-11-16 15:40:00','basement lol','','blue',''),(35,'test event','2016-11-16 13:50:33','2016-11-16 15:40:00','basement lol','','blue',''),(36,'test event','2016-11-16 13:50:33','2016-11-16 15:40:00','basement lol','','blue',''),(37,'test event','2016-11-16 13:50:33','2016-11-16 15:40:00','basement lol','','blue',''),(39,'best event','2016-12-16 13:50:33','2016-12-16 15:40:00','HNB','','red',''),(40,'best event','2016-12-16 13:50:33','2016-12-16 15:40:00','HNB','','red',''),(41,'New Event!','2016-11-16 14:48:37','2016-11-16 14:48:37','','','blue',''),(43,'gautams event','2016-11-20 12:00:00','2016-11-20 14:00:00','my butt','lol','magenta','\0');
 /*!40000 ALTER TABLE `Events` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -145,7 +206,7 @@ CREATE TABLE `Exams` (
   PRIMARY KEY (`exam_id`),
   KEY `FK_Exams_USCClasses` (`class_id`),
   CONSTRAINT `FK_Exams_USCClasses` FOREIGN KEY (`class_id`) REFERENCES `USCClasses` (`class_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -154,6 +215,7 @@ CREATE TABLE `Exams` (
 
 LOCK TABLES `Exams` WRITE;
 /*!40000 ALTER TABLE `Exams` DISABLE KEYS */;
+INSERT INTO `Exams` VALUES (1,1,'2016-12-02 19:00:00','2016-12-02 21:00:00');
 /*!40000 ALTER TABLE `Exams` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -181,6 +243,7 @@ CREATE TABLE `Friendships` (
 
 LOCK TABLES `Friendships` WRITE;
 /*!40000 ALTER TABLE `Friendships` DISABLE KEYS */;
+INSERT INTO `Friendships` VALUES (3,6,'pending'),(7,3,'accepted');
 /*!40000 ALTER TABLE `Friendships` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -206,7 +269,7 @@ CREATE TABLE `Labs` (
   UNIQUE KEY `Column 2` (`section_id`),
   KEY `FK_Discussions_USCClasses` (`class_id`),
   CONSTRAINT `Labs_ibfk_1` FOREIGN KEY (`class_id`) REFERENCES `USCClasses` (`class_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -215,6 +278,7 @@ CREATE TABLE `Labs` (
 
 LOCK TABLES `Labs` WRITE;
 /*!40000 ALTER TABLE `Labs` DISABLE KEYS */;
+INSERT INTO `Labs` VALUES (1,30238,1,'12:00:00','13:50:00','\0','\0','','\0','\0'),(2,30241,1,'18:00:00','19:50:00','\0','','\0','\0','\0');
 /*!40000 ALTER TABLE `Labs` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -232,7 +296,7 @@ CREATE TABLE `Notifications` (
   `notification_type` enum('email','online') NOT NULL DEFAULT 'online',
   `completed` bit(1) NOT NULL DEFAULT b'0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -241,6 +305,7 @@ CREATE TABLE `Notifications` (
 
 LOCK TABLES `Notifications` WRITE;
 /*!40000 ALTER TABLE `Notifications` DISABLE KEYS */;
+INSERT INTO `Notifications` VALUES (1,3,40,'email',''),(2,3,40,'email','');
 /*!40000 ALTER TABLE `Notifications` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -304,7 +369,7 @@ CREATE TABLE `USCClasses` (
   `friday` bit(1) NOT NULL DEFAULT b'0',
   PRIMARY KEY (`class_id`),
   UNIQUE KEY `Column 2` (`section_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -313,6 +378,7 @@ CREATE TABLE `USCClasses` (
 
 LOCK TABLES `USCClasses` WRITE;
 /*!40000 ALTER TABLE `USCClasses` DISABLE KEYS */;
+INSERT INTO `USCClasses` VALUES (1,29909,'CSCI',201,20163,'Principles of Software Development','Jeffrey Miller Ph.D.','WPH B27','09:30:00','10:50:00','\0','','\0','','\0');
 /*!40000 ALTER TABLE `USCClasses` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -332,7 +398,7 @@ CREATE TABLE `Users` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -341,7 +407,7 @@ CREATE TABLE `Users` (
 
 LOCK TABLES `Users` WRITE;
 /*!40000 ALTER TABLE `Users` DISABLE KEYS */;
-INSERT INTO `Users` VALUES (2,'test','537937FF92398C4454ECDE0E0DCFEB74CDAA780C776589BC31F59290235F36B920D733D59A6F87AA57051E999D4C514CFBC4A680C0DF2711BFA89B0DFAFC154B','swag@swag.com','2016-11-13 03:31:06'),(3,'vadim','0D2F44BE8166D5812502A6A908E2E695A5686DE9300B93BC2AF415915F80A5A61F342DB9089B85FE1F2F9F96130BC1056E5AF51EB802BC14C206632C2A4B65BC','vkorolik@gmail.com','2016-11-13 03:51:12');
+INSERT INTO `Users` VALUES (2,'test','537937FF92398C4454ECDE0E0DCFEB74CDAA780C776589BC31F59290235F36B920D733D59A6F87AA57051E999D4C514CFBC4A680C0DF2711BFA89B0DFAFC154B','swag@swag.com','2016-11-13 03:31:06'),(3,'vadim','0D2F44BE8166D5812502A6A908E2E695A5686DE9300B93BC2AF415915F80A5A61F342DB9089B85FE1F2F9F96130BC1056E5AF51EB802BC14C206632C2A4B65BC','vkorolik@gmail.com','2016-11-13 03:51:12'),(6,'ayylmao','0BA388B050F946467054A2339207B40DA1CAA81C1F226589F099B0A6A58482673659BD81082A0A42A4A7FBE2D1507B4D4B3759C23A70D052C211AF4A289398DC','swag@swag.swag','2016-11-15 06:39:58'),(7,'gautam','07BB9FCF5C104457F1B09C7CA92E4ABE87EF46AD6E10301E4BCA676658CCF406C4394EAB969C40DF695D5BBAC8332C414B8C25EB9D0B21A87EC465A2C2C1ACE2','gparanja@usc.edu','2016-11-17 07:40:06'),(8,'john','2E11705A4BEF4FD9F9E07EB9C5D5BF05C4A8458B7C8750BAB475F24C311774A5C8501BF26AAD9548BFF6D6889C77FBEA068E89C98E52B8B2A2E2E0640CD1EC95','jjohn12sm@gmail.com','2016-11-18 22:26:54');
 /*!40000 ALTER TABLE `Users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -354,4 +420,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-11-13 21:51:26
+-- Dump completed on 2016-11-20  0:01:34

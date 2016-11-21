d21db375071691395f23adabafe3de5c0994a554
CliffLee <leecliff@usc.edu>
2016-11-20 16:07:44 -0800

    Additional create event functionality

--- app/createEvent.js
+++ app/createEvent.js
@@ -1,82 +1,57 @@
 /* createEvent.js */
 
-var friendSource = [
-    "Clifford Lee",
-    "Christopher Lee",
-    "Samuel Breck",
-    "Samantha Brech",
-    "Gautam Paranjape",
-    "Gotham Bold",
-    "Vadim Korolik",
-    "Vladim Kouroboros",
-    "Prachi Nawathe",
-    "Pranav Nowaythe"
-]
+var friendDatalist  = document.getElementById("modal-createEvent-friends-list");
+var friendInput     = document.getElementById("modal-createEvent-friends-searchbar");
 
-var friendDatalist = document.getElementById("modal-createEvent-friends-list");
-var friendInput = document.getElementById("modal-createEvent-friends-searchbar");
+var title           = document.getElementById("eventTitle");
+var startTime       = document.getElementById("startTime");
+var startDate       = document.getElementById("startDate");
+var endTime         = document.getElementById("endTime");
+var endDate         = document.getElementById("endDate");
+var color           = document.getElementById("eventColor");
+var loc             = document.getElementById("eventLocation");
+var description     = document.getElementById("eventDescription");
 
-var title = document.getElementById("eventTitle");
-var startTime = document.getElementById("startTime");
-var startDate = document.getElementById("startDate");
-var endTime = document.getElementById("endTime");
-var endDate = document.getElementById("endDate");
-var loc = document.getElementById("eventLocation");
-var description = document.getElementById("eventDescription");
+/* populate friends data list for event creation/sharing */
 
+$.get("/friends", function(data) {
+    if (JSON.parse(data).success === true) {
+        var friends = JSON.parse(data).friends;
+        for (var x in friends) {
+            if (friends[x].status === "accepted") {
 
+                var div = document.createElement("div");
+                var option = document.createElement('option');
+                option.value = friends[x].username;
+                friendDatalist.appendChild(option);
 
-// FOR WHEN WE DO THE BACKEND FRIENDLIST INTEGRATION
-
-var request = new XMLHttpRequest();
-
-request.onreadystatechange = function(res) {
-    if (request.readyState === 4) {
-        if (request.status === 200 && res.target.response.success === true) {
-            var jsonOptions = JSON.parse(request.responseText);
-
-            jsonOptions.forEach(function(item) {
-                if (item.status === 'accepted') {
-                    var option = document.createElement('option');
-                    option.value = item.username;
-                    friendDatalist.appendChild(option);
-                }
-            });
-            friendInput.placeholder = "Search through friends";
-        } else {
-            friendInput.placeholder = "Couldn't load friends :o(";
+            }
         }
     }
-};
-
-request.open('GET', '/friends', true);
-request.send();
-
-
-/* Updated AJAX requests */
-
-
+});
 
 /* event creation -> angular scope */
 
-
-
-
 function create() {
+    if (!validateForm()) {
+        alert("Form was not completed!");
+        return false;
+    }
     var startDateArr = startDate.value.split("-");
     var endDateArr = endDate.value.split("-");
     var startTimeArr = startTime.value.split(":");
     var endTimeArr = endTime.value.split(":");
     var scope = angular.element($('#bodyTagID')).scope();
 
+    console.log("create() todo hardcoded sections");
 
     var event = {
-        "id": scope.getEventsLength(),
         "title": title.value,
         "start": new Date(startDateArr[0], startDateArr[1]-1, startDateArr[2], startTimeArr[0], startTimeArr[1]),
         "end": new Date(endDateArr[0], endDateArr[1]-1, endDateArr[2], endTimeArr[0], endTimeArr[1]),
         "location": loc.value,
         "description": description.value,
+        "color": "#FF00FF",
         "friends": [],
         "stick": true
     };
@@ -87,7 +62,7 @@ function create() {
         "end_time": endDate.value + " " + endTime.value + ":00",
         "location": loc.value,
         "description": description.value,
-        "color": "green",               // EDIT HARDCODE
+        "color": "#FF00FF",               // EDIT HARDCODE
         "relationship": "owned",        // EDIT HARDCODE
         "notify": true                  // EDIT HARDCODE
     }
@@ -98,6 +73,16 @@ function create() {
 
     $('#modal').html("<div></div>")
     $('#modal').toggleClass("modal-active");
+}
+
+
+/* form validation function */
+function validateForm()
+{
+    if (title.value == "" || startTime.value == "" || endTime.value == "" || startDate.value == "" || endDate.value == "" || color.value == "" || loc.value == "" || description.value == "") {
+        return false;
+    }
+    return true;
 }
 
 /* createEvent.js EOF */

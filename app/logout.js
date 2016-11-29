4864bbca1f0da8d34a9a782b7a3b7ee4d4ab15a0
CliffLee <leecliff@usc.edu>
2016-11-29 00:18:46 -0800

    fix for logout->not able to open modal bug

--- app/logout.js
+++ app/logout.js
@@ -1,13 +1,15 @@
 // logout.js
 
 function logoutUser() {
-    
+
     $.ajax({
             url : "/user/logout",
             type: "POST",
             success: function(data, jqXHR)
             {
                 window.location.reload();
+                $('#modal').html("<div></div>")
+                $('#modal').toggleClass("modal-active");
             },
             error: function(jqXHR, textStatus, errorThrown)
             {
